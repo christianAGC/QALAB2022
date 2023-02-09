723b44b09d19316e3532aa863f2d32f7be60d9c9
christianAGC <124703095+christianAGC@users.noreply.github.com>
2023-02-09 18:19:46 -0500

    editado por Christian Garcia

--- POO_Game/src/Main.java
+++ POO_Game/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        //editado por Christian Garcia
 
         Toyota toyo = new Toyota("PAS201"); //200
         toyo.encenderMotor();
